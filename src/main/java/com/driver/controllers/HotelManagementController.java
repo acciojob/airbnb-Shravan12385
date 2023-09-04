542aba056c610b4e2f41f8280ec7efc2e243371d
shravan reddy <shravankolla1976@gmail.com>
2023-09-04 21:34:13 +0530

    assignment

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -1,11 +1,9 @@
 package com.driver.controllers;
 
-import com.driver.Service.HotelManagementSerivce;
 import com.driver.model.Booking;
 import com.driver.model.Facility;
 import com.driver.model.Hotel;
 import com.driver.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,8 +20,9 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/hotel")
 public class HotelManagementController {
-    @Autowired
-    HotelManagementSerivce hotelManagementSerivce;
+
+    HotelManagementService hotelManagementService = new HotelManagementService();
+
     @PostMapping("/add-hotel")
     public String addHotel(@RequestBody Hotel hotel){
 
@@ -31,16 +30,9 @@ public class HotelManagementController {
         //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
         //Incase somebody is trying to add the duplicate hotelName return FAILURE
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
-        hotelManagementSerivce.addHotel(hotel);
-        if(hotel.getHotelName()==null || hotel==null )
-        {
 
-            return "an empty a FAILURE";
-        }
-        else
-        {
-            return   hotelManagementSerivce.addHotel(hotel);
-        }
+
+        return hotelManagementService.addHotel(hotel);
     }
 
     @PostMapping("/add-user")
@@ -48,14 +40,8 @@ public class HotelManagementController {
 
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
-        if(user!=null)
-        {
-            hotelManagementSerivce.addUser(user);
-            return user.getaadharCardNo();
-        }
 
-
-       return 0;
+        return hotelManagementService.addUser(user);
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -65,8 +51,7 @@ public class HotelManagementController {
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
 
-        if(hotelManagementSerivce.getHotelWithMostFacilities()!=null) return  hotelManagementSerivce.getHotelWithMostFacilities().getHotelName();
-        else return "";
+        return hotelManagementService.getHotelWithMostFacilities();
     }
 
     @PostMapping("/book-a-room")
@@ -76,20 +61,17 @@ public class HotelManagementController {
         //Have bookingId as a random UUID generated String
         //save the booking Entity and keep the bookingId as a primary key
         //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
-        //If there arent enough rooms available in the hotel that we are trying to book return -1 
+        //If there arent enough rooms available in the hotel that we are trying to book return -1
         //in other case return total amount paid
 
-
-        
-        return 0;
+        return hotelManagementService.bookARoom(booking);
     }
-    
+
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
         //In this function return the bookings done by a person
-        return hotelManagementSerivce.getBooking(aadharCard);
-
+        return hotelManagementService.getBookings(aadharCard);
     }
 
     @PutMapping("/update-facilities")
@@ -99,8 +81,7 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
-       return  hotelManagementSerivce.putHotel(newFacilities,hotelName);
-
+        return hotelManagementService.updateFacilities(newFacilities, hotelName);
     }
 
 }
